7dad1abe3a51f81b9fcade1efade03f1d0663371
glbb666 <menghuangjingy@126.com>
2019-04-15 20:51:06 +0800

    。

--- js/index.js
+++ js/index.js
@@ -1,4 +1,22 @@
-// let list = document.getElementsByClassName('swiper-pagination-bullet');
-// for(let item of list){
-//   item.innerHTML = '';
+var list = document.getElementsByClassName('swiper-pagination-bullet')
+var swiper = document.getElementsByClassName('swiper-slide')
+// var zhou = document.getElementById('zhou');
+// var qin = document.getElementById('qin');
+// var han = document.getElementById('han');
+// var tang = document.getElementById('tang');
+// var song = document.getElementById('song');
+// var ming = document.getElementById('ming');
+// var arr = ['slideLeft','fadeIn','zoomer','bounceIn','rotateIn','slideDown'];
+// console.log(list);
+// console.log(swiper);
+// for(let i = 0;i<list.length;i++){
+//     list[i].addEventListener("slideChangeTransitionStart",function(){
+//         var div = swiper[i+1].getElementsByTagName('div');
+//         console.log(div[1]);
+//         div[1].classList.add(arr[i]);
+//         var timer = setTimeout(function(){
+            
+//         },3000);
+//     }) 
 // }
+
