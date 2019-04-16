818e8903c81398e15d42b27f926841ce03237c53
glbb666 <menghuangjingy@126.com>
2019-04-16 22:43:36 +0800

    .

--- js/index.js
+++ js/index.js
@@ -1,22 +1,15 @@
 var list = document.getElementsByClassName('swiper-pagination-bullet')
 var swiper = document.getElementsByClassName('swiper-slide')
-// var zhou = document.getElementById('zhou');
-// var qin = document.getElementById('qin');
-// var han = document.getElementById('han');
-// var tang = document.getElementById('tang');
-// var song = document.getElementById('song');
-// var ming = document.getElementById('ming');
-// var arr = ['slideLeft','fadeIn','zoomer','bounceIn','rotateIn','slideDown'];
-// console.log(list);
-// console.log(swiper);
-// for(let i = 0;i<list.length;i++){
-//     list[i].addEventListener("slideChangeTransitionStart",function(){
-//         var div = swiper[i+1].getElementsByTagName('div');
-//         console.log(div[1]);
-//         div[1].classList.add(arr[i]);
-//         var timer = setTimeout(function(){
-            
-//         },3000);
-//     }) 
-// }
+var ani = swiper[5].getElementsByTagName('span');
+var cover = swiper[5].getElementsByTagName('h6');
+console.log(ani);
+for(let i = 1;i<8;i+=2){
+    ani[i].onmouseover = function(){
+        ani[i].classList.add('heartbeat')
+    }
+    ani[i].onmouseout = function(){
+        ani[i].classList.remove('heartbeat')
+    }
+}
 
+list[0].classList.add('rotation')
